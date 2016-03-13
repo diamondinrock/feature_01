57e53f006dff122497968b9cbf65c6fd81f96bd2
michelleli94 <michelle_dong_li@yahoo.com>
2016-03-13 15:41:58 -0700

    Check in Jessica's change into Sprint 1

--- sitedir/appuser/views.py
+++ sitedir/appuser/views.py
@@ -5,4 +5,22 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Welcome to user management interface for Diamond in Rough")
+	# context = {
+	# 	'teams_list': Teams.objects.all(), 
+	# 	'tasks_list': Tasks.objects.all()
+	# }
+	context = {
+		'teams_list': [
+			{'team_name':'Production', 'first_letter':'P'},
+			{'team_name':'Engineering', 'first_letter':'E'},
+			{'team_name':'Transportation', 'first_letter':'T'},
+			{'team_name':'Marketing', 'first_letter':'M'}
+		],
+		'tasks_list': [
+			{'task_name': 'I am a task', 'task_description': 'I am a task description', 'task_leader': 'Bernie Sanders', 'creation_date': '2016-01-01'},
+			{'task_name': 'I am also a task', 'task_description': 'I am also a task description', 'task_leader': 'Barack Obama', 'creation_date': '2016-01-02'},
+			{'task_name': 'I am the third task', 'task_description': 'I am the third task description', 'task_leader': 'Hilary Clinton', 'creation_date': '2016-01-03'}
+		]
+	}
+
+	return render(request, 'appuser/index.html', context)
