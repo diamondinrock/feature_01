1ae1ee0d1fc2937bb684f8a739b4213f2949ba93
Michelle <m.l.i.u9333@gmail.com>
2016-03-15 22:35:32 -0400

    Update load_data_dir_teams.sql

--- tools/data/load_data_dir_teams.sql
+++ tools/data/load_data_dir_teams.sql
@@ -1,4 +1,4 @@
---Be sure your database is created based on the most current init.sql and alter_tables_timestamp.sql
+
 -- Be sure to place the dir_teams.txt in the right directory based on your database settings.
 
 LOAD DATA INFILE "dir_teams.txt"
