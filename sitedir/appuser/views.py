4cd127f59b07bd40a54344053bce016827070fbe
WX <rlaterson@gmail.com>
2016-03-16 07:18:49 -0400

    Update views.py

--- sitedir/appuser/views.py
+++ sitedir/appuser/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.http import HttpRequest
+import connector
+import json
 from . import wechatuser
 
 def index(request):
