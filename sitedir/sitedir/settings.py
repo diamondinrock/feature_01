1773efc6007906fc1051988bc6b2c0d9789552e8
jshi17 <weiqian.shi@gmail.com>
2016-03-24 16:16:32 -0400

    fixed typo

--- sitedir/sitedir/settings.py
+++ sitedir/sitedir/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'read_default_file': database_cnf_path
-        }
+        },
         'HOST': 'localhost',
         'PORT': '3306',
     }
