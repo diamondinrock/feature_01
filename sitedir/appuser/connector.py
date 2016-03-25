630f284ac966d7ef1e3112436071135ee48192c3
Michelle <m.l.i.u9333@gmail.com>
2016-03-25 14:09:25 -0400

    updated sql queries

--- sitedir/appuser/connector.py
+++ sitedir/appuser/connector.py
@@ -1,8 +1,10 @@
 import json
 from django.http import HttpResponse
 from django.core import serializers
-from .models import DirTeams#change depending on what models will be
-from .models import DirTasks 
+from .models import DirTeam#change depending sql
+from .models import DirTask
+from .models import DirTeamMember
+from .models import DirTaskAssignment #possibly incorrect names, must check
 from django.utils.encoding import force_text
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db import connection 
@@ -27,35 +29,35 @@ def getRecentTasks(request, number_of_tasks):
     
 def getNumMemberTasks(request): #b = task_id number #other variable parameters must check on
     cursor = connection.cursor()  #creates cursor
-    cursor.execute("select a.team_name, count(distinct(b.task_id)) from dir_teams a, dir_task_assignments b, dir_tasks c where a.team_id = b.team_id and c.task_id = b.task_id and DATEDIFF (c.signup_due_date,NOW()) > 1 group by b.team_id") #executes the sql quere
+    cursor.execute("select  a.team_id, b.team_name, a.task_id, a.task_name, count(c.person_id) from diamondrough.dir_task a, diamondrough.dir_team b, diamondrough.dir_task_assignment c where a.team_id = b.team_id and c.task_id = c.task_id group by b.team_id, a.task_id") #executes the sql quere
     numMemberTasks = cursor.fetchone()
     numMemberTasks_as_json = serializers.serialize('json', numMemberTasks.objects.all()) #must test - see if json will take sql results
     return numMemberTasks
     
 def getNumNewTasks(request):
     cursor = connection.cursor()  #creates cursor
-    cursor.execute("select %s.team_name, count(distinct(%s.task_id)) from dir_teams %s, dir_task_assignments %s where a.team_id = %s.team_id and DATEDIFF (NOW(), %s.modified_date)) < 7 group by %s.team_id", a,b,a,b,a,b,b,b) #executes the sql quere
+    cursor.execute("select a.team_id, a.team_name, count(distinct(b.task_id)) from dir_team a, dir_task b where a.team_id = b.team_id and DATEDIFF (b.signup_due_date,NOW()) > 1 group by b.team_id") #executes the sql 
     numNewTasks = cursor.fetchall()
     numNewTasks_as_json = serializers.serialize('json', numNewTasks.objects.all()) 
     return numNewTasks
     
 def getTotalTasks(request):
     cursor = connection.cursor()  #creates cursor
-    cursor.execute("select a.team_name, count(distinct(b.task_id)) from dir_teams a, dir_task_assignments b where a.team_id = b.team_id group by b.team_id") #executes the sql quere
+    cursor.execute("select  a.team_id, b.team_name, count(a.task_id) from diamondrough.dir_task a, diamondrough.dir_team b where a.team_id = b.team_id group by b.team_id") #executes the sql 
     totalTasks = cursor.fetchall()
     totalTasks_as_json = serializers.serialize('json', totalTasks.objects.all()) 
     return totalTasks
 
-def getTeamMembers(request):
+def getTeamMembers(request): #need team parameter
     cursor = connection.cursor()  #creates cursor
-    cursor.execute("select c.team_name, a.last_name, a.first_name from diamondrough.dir_personnel a, diamondrough.dir_person_position_assignments b, diamondrough.dir_teams c where a.person_id = b.person_id and b.team_id = c.team_id") #executes the sql query, must find out selecting team name parameter
+    cursor.execute("select c.team_id, c.team_name, a.last_name, a.first_name from diamondrough.dir_personnel a, diamondrough.dir_team_member b,  diamondrough.dir_team c where a.person_id = b.person_id  and b.team_id = c.team_id") #executes the sql query, must find out selecting team name parameter
     teamMembers = cursor.fetchall()
     teamMembers_as_json = serializers.serialize('json', teamMembers.objects.all()) #may not need json
     return teamMembers
 
 def getNamesTasks(request):
     cursor = connection.cursor()  #creates cursor
-    cursor.execute("select a.team_name, c.task_name from dir_teams a, dir_task_assignments b, dir_tasks c where a.team_id = b.team_id and c.task_id = b.task_id") #executes the sql quere
+    cursor.execute("select a.team_id, a.team_name, b.task_name from dir_team a,   dir_task b where a.team_id = b.team_id") #executes the sql 
     namesTasks = cursor.fetchall()
     namesTasks_as_json = serializers.serialize('json', namesTasks.objects.all()) #may not need json
     return namesTasks
