3292a3b5283e550fab8e82721e66f617ea1ef13e
bryanly200 <bryanly200@gmail.com>
2016-04-01 21:52:59 -0700

    team detail backend

--- sitedir/appuser/connector.py
+++ sitedir/appuser/connector.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from django.http import HttpResponse
 from django.core import serializers
 from .models import DirTeam#change depending sql
@@ -114,6 +115,55 @@ def getNamesTasks(request):
     namesTasks = cursor.fetchall()
     namesTasks_as_json = serializers.serialize('json', namesTasks.objects.all()) #may not need json
     return namesTasks
+    
+def getTeambyID(teamID):
+    teamdetail={}
+    
+    try:
+        DirTeamInfo = serializers.serialize('json', DirTeam.objects.filter(pk=teamID), fields=('team_name', 'team_description'))
+    except DirTeam.DoesNotExist:
+        return teamdetail
+    
+    try:
+        DirTeamLeader = serializers.serialize('json', DirPersonnel.objects.filter(dirteam__team_id__exact=teamID), fields=('user_name'))
+    except DirPersonnel.DoesNotExist:
+         DirTeamLeader = 'None'    
+    
+    try:
+        DirTeamMembers = serializers.serialize('json', DirPersonnel.objects.filter(dirteammember__team_id__exact=teamID), fields=('user_name'))
+        NumDirTeamMembers = DirPersonnel.objects.filter(dirteammember__team_id__exact=teamID).count()
+    except DirPersonnel.DoesNotExist:
+        NumDirTeamMembers = 0
+    
+    try:
+        DirTeamTasks = serializers.serialize('json', DirTask.objects.filter(team_id=teamID), fields=('task_name'))
+        NumDirTeamTasks = DirTask.objects.filter(team_id=teamID).count()
+    except DirTask.DoesNotExist:
+        NumDirTeamTasks = 0
+    
+    try:
+        DirNewTeamTasks = serializers.serialize('json', DirTask.objects.filter(team_id=teamID, signup_due_date__gt=datetime.date.today()))
+        NumDirNewTeamTasks = DirTask.objects.filter(team_id=teamID, signup_due_date__gt=datetime.date.today()).count()
+    except DirTask.DoesNotExist:
+        NumDirNewTeamTasks = 0
+        
+    teamdetail['team_name']=json.loads(DirTeamInfo)[0]['fields']['team_name']
+    teamdetail['team_leader']=json.loads(DirTeamLeader)[0]['fields']['user_name']
+    teamdetail['team_description']=json.loads(DirTeamInfo)[0]['fields']['team_description']
+    teamdetail['num_team_members']=NumDirTeamMembers
+    teamdetail['num_total_tasks']=NumDirTeamTasks
+    teamdetail['num_new_tasks']=NumDirNewTeamTasks
+    members=[]
+    if (NumDirTeamMembers > 0):
+        for user in json.loads(DirTeamMembers):
+            members.append(user['fields']['user_name'])
+    teamdetail['team_members']=members
+    tasks=[]
+    if (NumDirTeamTasks > 0):
+        for task in json.loads(DirTeamTasks):
+            tasks.append(task['fields']['task_name'])
+    teamdetail['team_tasks']=tasks
+    return teamdetail
     
 def getTaskbyID(taskID):
     DirPeopleInTask = serializers.serialize('json', DirPersonnel.objects.filter(dirtaskassignment__task_id__exact=taskID), fields=('user_name'))
