cdbcef1c541fe1d4f3ef053fe42eeee4725495a9
WX <rlaterson@gmail.com>
2016-04-09 20:04:47 -0400

    Fixes Url that was overwritten by merge

--- sitedir/appuser/urls.py
+++ sitedir/appuser/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^userinfo', views.wechat, name='wechat'),
     url(r'allteams', views.allteams, name='allteams'),
+    url(r'^team/(?P<teamid>\w{0,50})/$', views.teams),
     url(r'^task/(?P<taskid>\d+)/$', views.taskdetail, name='taskdetail'),
 ]
