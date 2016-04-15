6f59fbeafbe5a846dc44a4c4898b576b96bd1bec
Yucheng Zhao <zhaoyucheng@live.cn>
2016-04-16 00:34:31 +0800

    Revert "Revert "Update all-task function and page""
    This reverts commit 5ffdc725f095cfa1c2fed55c0acd9c6a6fac44ac.

--- sitedir/appuser/urls.py
+++ sitedir/appuser/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^userinfo', views.wechat, name='wechat'),
     url(r'allteams', views.allteams, name='allteams'),
+    url(r'alltasks', views.alltasks, name='alltasks'),
     url(r'^team/(?P<teamid>\d+)/$', views.teams),
     url(r'^task/(?P<taskid>\d+)/$', views.taskdetail, name='taskdetail'),
 ]
