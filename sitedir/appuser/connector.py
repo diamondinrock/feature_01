e230b0379283a8ee84bc679936b1f3fd16d232f3
Michelle <m.l.i.u9333@gmail.com>
2016-04-24 21:14:54 -0400

    updated connector.py
    reverted back to former completedTasks

--- sitedir/appuser/connector.py
+++ sitedir/appuser/connector.py
@@ -243,9 +243,6 @@ def getPersonalProfile(personID):
         personalprofile
     try:
         completedTasks = DirTask.objects.filter(person_id=personID, completion_date__gt = datetime.date(year=year,month=month,day=day,hour=hour)
-        completeTasks = []
-        for completeTask in completedTasks
-            completeTasks.append([completedTasks])
         personalprofile['completed_tasks'] = completedTasks
     except DirTask.DoesNotExist:
         personalprofile['completed_tasks'] = None
