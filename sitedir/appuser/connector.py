0a553ad01da394086af5457bcd39b160e9480d63
cihtsliu <liuc@d-e.org>
2016-05-15 10:45:29 -0400

    Update connector.py

--- sitedir/appuser/connector.py
+++ sitedir/appuser/connector.py
@@ -296,6 +296,14 @@ def updateEmploymentHistoryData(person_id, employer_name, employment_start_date,
         return -1
     return 1
 
+def getRecentEducation(person_id):
+    data = {}
+    educations = getEducationHistoryByPersonnel(person_id)
+    if not educations:
+        return data
+    data = educations[0]
+    return data
+
 def getEducationHistoryByPersonnel(person_id):
     data = []
     education_history = DirEducationHistory.objects.filter(person_id=person_id).order_by('-college_start_date')
